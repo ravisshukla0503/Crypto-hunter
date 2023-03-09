827725c9bc3622411c9dce7255c2358aa6878da9
Ravi Shanker Shukla <ravisshukla0503>
2023-03-09 21:07:05 +0530

    crypto-hunter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CoinPage from './pages/CoinPage';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/coins/:id' element={<CoinPage />} />
+      </Routes>
     </div>
   );
 }
